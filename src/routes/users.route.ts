f1419da002e9815f5f61e040a1a9882bd849f2d1
Afonso Machado <afonso.machado@blockhub.builders>
2021-12-30 09:50:28 -0300

    rotas put e delete

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -21,6 +21,22 @@ usersRoute.post("/users", (req: Request, res: Response, next: NextFunction) => {
   res.status(StatusCodes.CREATED).send(newUser);
 });
 
+usersRoute.put(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({ uuid });
+  }
+);
+
+usersRoute.delete(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.sendStatus(StatusCodes.OK);
+  }
+);
+
 export default usersRoute;
 
 /**
