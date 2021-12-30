01e922e2348f615569bda69c125d9772e17faca7
Afonso Machado <afonso.machado@blockhub.builders>
2021-12-30 09:06:50 -0300

    primeira requisição get status

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("opaaaa");
+import express, { Request, Response, NextFunction } from "express";
+
+const port = 3000;
+
+//* biblioteca para o gerenciamento de rotas HTTP
+const app = express();
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: "bar" });
+});
+
+app.listen(port, () => {
+  console.log(`API executando na porta ${port}`);
+});
