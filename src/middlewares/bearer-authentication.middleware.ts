7e178146a813f65b88d332b25b38e4662a5cd7ea
Afonso Machado <afonso.machado@blockhub.builders>
2021-12-30 17:42:18 -0300

    busca de usuarios validada por toke

---
+++ src/middlewares/bearer-authentication.middleware.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import ForbiddenError from "../errors/forbidden.error";
+import JWT from "jsonwebtoken";
+import userRespository from "../respositories/user.respository";
+
+function bearerAuthenticationMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const authorizationHeader = req.headers["authorization"];
+
+    if (!authorizationHeader) {
+      throw new ForbiddenError("Credenciais não informadas");
+    }
+
+    const [authenticationType, token] = authorizationHeader.split(" ");
+
+    // Veficica o tipo da autenticação se é do tipo bearer
+    if (authenticationType !== "Bearer" || !token) {
+      throw new ForbiddenError("Tipo de autenticação inválido");
+    }
+
+    // Verifica se o token é válido, se for valido retorna o payload do token
+    const tokenPayload = JWT.verify(token, "my_secret_key");
+
+    if (typeof tokenPayload !== "object" || !tokenPayload.sub) {
+      throw new ForbiddenError("Token inválido");
+    }
+
+    const uuid = tokenPayload.sub;
+    // user que está dentro do token
+    const user = {
+      uuid: tokenPayload.sub,
+      username: tokenPayload.username,
+    };
+    // adicionando o user no corpo da requisição
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+export default bearerAuthenticationMiddleware;
