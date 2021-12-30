264d4d317281b75cb3d186b8972ac2bc3a71dc75
Afonso Machado <afonso.machado@blockhub.builders>
2021-12-30 10:25:19 -0300

    consultas para o banco de dados

---
+++ sql/init.sql
@@ -0,0 +1,13 @@
+-- Habilitando extensão para a função uuid_generate_v4()
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- Extensão para usar a função crypt()
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS application_user (
+    uuid uuid DEFAULT uuid_generate_v4(),
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    PRIMARY KEY (uuid)
+);
+
+INSERT INTO application_user (username, password) VALUES ('admin', crypt('admin', 'my_salt'));
