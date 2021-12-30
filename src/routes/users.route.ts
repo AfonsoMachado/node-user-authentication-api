23d4a21db239f392391ebed682ec7ed20ac07b18
Afonso Machado <afonso.machado@blockhub.builders>
2021-12-30 09:35:03 -0300

    padronizando status code

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,17 +1,18 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const usersRoute = Router();
 
 usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
   const users = [{ username: "afonso" }];
-  res.status(200).send(users);
+  res.status(StatusCodes.OK).send(users);
 });
 
 usersRoute.get(
   "/users/:uuid",
   (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(200).send({ uuid });
+    res.status(StatusCodes.OK).send({ uuid });
   }
 );
 
